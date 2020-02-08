19a134e48b6e709bf2eedaf18c402e07ed6fcda5
Mateus <mateusfreitas16@hotmail.com>
2020-02-08 20:12:09 -0300

    aplicativo youtube finalizado

---
+++ youtube/lib/Api.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:youtube/model/Video.dart';
+
+const CHAVE_YOUTUBE_API = "AIzaSyBJohGPdPpiZ6Pvjqw9O8NHTj28Z4U8dwQ";
+const ID_CANAL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
+const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+
+class Api{
+
+  Future<List<Video>> pesquisar(String pesquisa) async{
+    http.Response response = await http.get(URL_BASE + "search"  "?part=snippet"+ "&type=video"
+        "&maxResults=20" "&order=date"  "&key=$CHAVE_YOUTUBE_API"  "&channelId=$ID_CANAL"
+        "&q=$pesquisa"
+    );
+
+    if(response.statusCode == 200){
+      Map<String,dynamic> dadosJson = json.decode(response.body);
+      List<Video> videos = dadosJson["items"].map<Video>(
+          (map){
+            return Video.fromJson(map);
+          }
+      ).toList();
+
+      return videos;
+
+    }else{
+
+    }
+
+
+  }
+}
