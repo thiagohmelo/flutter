666b73e259bbab0aa1152964c45bb98fe29deee7
Mateus <mateusfreitas16@hotmail.com>
2020-02-29 21:34:06 -0300

    aprenda ingles

---
+++ aprenda_ingles/lib/telas/Home.dart
@@ -0,0 +1,62 @@
+import 'package:aprenda_ingles/telas/Bichos.dart';
+import 'package:aprenda_ingles/telas/Numeros.dart';
+import 'package:aprenda_ingles/telas/Vogais.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(
+      length: 3,
+      vsync: this
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aprenda inglês"),
+        bottom: TabBar(
+          indicatorWeight: 4,
+          indicatorColor: Colors.white,
+          labelStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(text: "Bichos",),
+            Tab(text: "Números",),
+            Tab(text: "Vogais",)
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Bichos(),
+          Numeros(),
+          Vogais()
+        ],
+      ),
+    );
+  }
+}
