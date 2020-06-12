b563008d9b0425a050a7aec6177012d8c18bd917
C1314327 - MATEUS FREITAS <c1314327@bb.com.com>
2020-06-12 20:07:40 -0300

    app viagens finalizado

---
+++ minhas_viagens/android/app/src/main/kotlin/br/com/minhasviagens/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.minhasviagens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ minhas_viagens/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:minhasviagens/SplashScreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Minhas Viagens",
+    home: SplashScreen(),
+  ));
+}
