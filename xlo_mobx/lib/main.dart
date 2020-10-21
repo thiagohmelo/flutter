7149af38fa6a70ecbd0241c52cf18e1f192e038d
Mateus freitas <mateusfreitas16@hotmail.com>
2020-10-20 22:45:23 -0300

    projeto parcial

---
+++ xlo_mobx/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:xlo_mobx/screens/base/base_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
+import 'package:xlo_mobx/stores/user_manager_store.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeParse();
+  setupLocators();
+  runApp(MyApp());
+}
+
+void setupLocators() {
+  GetIt.I.registerSingleton(PageStore());
+  GetIt.I.registerSingleton(UserManagerStore());
+}
+
+Future<void> initializeParse() async {
+  await Parse().initialize(
+    'g7SpGW6DZVnjVtFnxGWRkFK9GzWPe5Nd3XpZWl6d',
+    'https://parseapi.back4app.com/',
+    clientKey: 'VfbVYwkN86nRoh5QdLoEvDRxv7tTujHsRuzwSgN4',
+    autoSendSessionId: true,
+    debug: true,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'XLO',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.purple,
+        appBarTheme: AppBarTheme(
+          elevation: 0
+        ),
+        cursorColor: Colors.orange
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
---
+++ xlo_mobx/lib/screens/login/login_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:xlo_mobx/components/error_box.dart';
+import 'package:xlo_mobx/screens/signup/signup_screen.dart';
+import 'package:xlo_mobx/stores/login_store.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  final LoginStore loginStore = LoginStore();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Entrar'),
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Card(
+              margin: const EdgeInsets.symmetric(horizontal: 32),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16)
+              ),
+              elevation: 8,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Acessar com E-mail:',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[900],
+                      ),
+                    ),
+                    Observer(builder: (_){
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: ErrorBox(
+                          message: loginStore.error,
+                        ),
+                      );
+                    }),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 3, bottom: 4, top: 8),
+                      child: Text(
+                        'E-mail',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    Observer(builder: (_){
+                      return TextField(
+                        enabled: !loginStore.loading,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          isDense: true,
+                          errorText: loginStore.emailError,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: loginStore.setEmail,
+                      );
+                    }),
+                    const SizedBox(height: 16,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 3, bottom: 4),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Senha',
+                            style: TextStyle(
+                              color: Colors.grey[800],
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              'Esqueceu sua senha?',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.purple,
+                              ),
+                            ),
+                            onTap: (){},
+                          )
+                        ],
+                      ),
+                    ),
+                    Observer(builder: (_){
+                      return TextField(
+                        enabled: !loginStore.loading,
+                        decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            isDense: true,
+                            errorText: loginStore.passwordError
+                        ),
+                        obscureText: true,
+                        onChanged: loginStore.setPassword,
+                      );
+                    }),
+                    Observer(builder: (_){
+                      return Container(
+                        height: 40,
+                        margin: const EdgeInsets.only(top: 20, bottom: 12),
+                        child: RaisedButton(
+                          color: Colors.orange,
+                          disabledColor: Colors.orange.withAlpha(120),
+                          child: loginStore.loading
+                            ? CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation(Colors.white),
+                            )
+                            : Text('ENTRAR'),
+                          textColor: Colors.white,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                          onPressed: loginStore.loginPressed,
+                        ),
+                      );
+                    }),
+                    Divider(color: Colors.black,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'Não tem uma conta? ',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(builder: (_) => SignUpScreen())
+                              );
+                            },
+                            child: Text(
+                              'Cadastre-se',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.purple,
+                                fontSize: 16,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
