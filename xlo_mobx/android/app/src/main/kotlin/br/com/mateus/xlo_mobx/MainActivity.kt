7f6dd92c2f9e64fb06105640434ea7dd004772d6
Mateus freitas <mateusfreitas16@hotmail.com>
2020-10-21 08:29:52 -0300

    aplicativo corrigido

---
+++ xlo_mobx/android/app/src/main/kotlin/br/com/mateus/xlo_mobx/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.mateus.xlo_mobx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xlo_mobx/lib/screens/base/base_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/screens/create/create_screen.dart';
+import 'package:xlo_mobx/screens/home/home_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
+
+class BaseScreen extends StatefulWidget {
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  final PageController pageController = PageController();
+
+  final PageStore pageStore = GetIt.I<PageStore>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    reaction((_) => pageStore.page, (page) => pageController.jumpToPage(page));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          HomeScreen(),
+          CreateScreen(),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.purple,
+          ),
+          Container(
+            color: Colors.brown,
+          ),
+        ],
+      ),
+    );
+  }
+}
